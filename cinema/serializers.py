c4a7e4c1e533b75ba2257bf8cd4336a2e234489d
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-02-09 16:35:06 +0200

    Fix MovieSessionListSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -66,7 +66,7 @@ class MovieSessionSerializer(serializers.ModelSerializer):
         fields = ("id", "show_time", "movie", "cinema_hall")
 
 
-class MovieSessionListSerializer(MovieSessionSerializer):
+class MovieSessionListSerializer(serializers.ModelSerializer):
     movie_title = serializers.CharField(
         source="movie.title",
         read_only=True
