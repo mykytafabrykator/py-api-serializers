8c4401eeecee723c2e1e902f68fe132276a97a9f
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-02-09 17:41:28 +0200

    Update returns in views, update urls in cinema app

--- cinema/views.py
+++ cinema/views.py
@@ -44,7 +44,7 @@ class MovieViewSet(viewsets.ModelViewSet):
             return MovieListSerializer
         if self.action == "retrieve":
             return MovieRetrieveSerializer
-        return MovieSerializer
+        return self.serializer_class
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
@@ -62,4 +62,4 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionListSerializer
         if self.action == "retrieve":
             return MovieSessionRetrieveSerializer
-        return MovieSessionSerializer
+        return self.serializer_class
